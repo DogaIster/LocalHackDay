c25dd8f1b94bebca5c5544c657ed43d7e95faa23
Doga Ister <dogaister@Dogas-MacBook-Pro.local>
2017-12-02 12:29:49 -0500

    editor for the frontend and gitignore for the main folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 import { AppComponent } from './app.component';
 
@@ -10,8 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
+],
   providers: [],
   bootstrap: [AppComponent]
 })
