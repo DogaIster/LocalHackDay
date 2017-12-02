9833443084cd923f9237f5a447718c5c89ea0844
suoken <kensuong@gmail.com>
2017-12-02 12:08:46 -0500

    PDFKit aded to generate a pdf

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const http = require('http');
+var pdf = require('pdfkit'); 
+var fs = require('fs'); 
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+// make the pdf
+var myDoc = new pdf; 
+myDoc.pipe(fs.createWriteStream('node.pdf')); 
+myDoc.font('Times-Roman')
+    .fontSize(48)
+    .text('NodeJS PDF Document', 100, 100); 
+myDoc.end(); 
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
